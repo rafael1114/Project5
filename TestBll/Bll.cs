9600bb695891bc68e7eeb8ff2c8f6067ad24223c
rafael1114 <37363448+rafael1114@users.noreply.github.com>
2018-05-02 00:49:42 -0500

    Add files via upload

---
+++ TestBll/Bll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDALEF;
+using TestEntities;
+namespace TestBll
+{
+    public class Bll
+    {
+        Dal d = new Dal();
+
+        public List<PersonView> GetPersons()
+        {
+            PersonView p = new PersonView();
+            List<PersonView> pers = new List<PersonView>();
+            List<Person> personlist = d.GetPersons();
+            foreach (Person per in personlist)
+            {
+                p = new PersonView { ID = per.PersonID, Name = per.Name };
+                pers.Add(p);
+            }
+            return pers;
+        }
+    }
+}
